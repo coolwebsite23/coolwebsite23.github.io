8d2b7886bbc4b5ebe522ce334efd90493a2c2e5c
coolwebsite23 <98031434+coolwebsite23@users.noreply.github.com>
2022-01-19 10:11:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-document.title = "Home Page";
-var h1 document.createElement("h1");
-h1.innerText = document.title;
-document.body.appendChild(h1);
-var hr = document.createElement("hr");
-var hrp = document.createElement("p");
-hrp.innerText = "Some Text";
-var hr2 = document.createElement("hr");
-document.body.appendChild(hr, hrp, hr2);
+document.title = "HOME PAGE";
+document.getElementById("title").innerHTML = document.title;
