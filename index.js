13d287ddda6874e5fc3fc947fea4679da6d89277
coolwebsite23 <98031434+coolwebsite23@users.noreply.github.com>
2022-01-19 11:29:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ document.title = "HOME PAGE";
 function rand(){
   document.write(Math.floor(Math.random() * 1000));
 }
+document.body.style.display ="none";
+setTimeout(function(){
+  document.body.style.display = "block";
+}, 5000);
