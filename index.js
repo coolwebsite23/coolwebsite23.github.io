e238835093f7b39908fd58c97802f0e561cbaadc
coolwebsite23 <98031434+coolwebsite23@users.noreply.github.com>
2022-01-19 11:42:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ function links(){
   document.getElementById("links").style.display = "block";
 }
 if(document.body.onerror){
-   var script = prompt("do you want to turn off JavaScript for this website? (true or false)");
+   var script = prompt("do you want to turn off JavaScript for this website? (true or false) there has been an error found on the code!");
    if(script == true){
      debugger;
    }else{
      console.log("no");
    }
 }
-document.body.onerror = true;
