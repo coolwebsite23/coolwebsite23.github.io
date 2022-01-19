b4cd15f33f3dd340421906817fc1cac676595903
coolwebsite23 <98031434+coolwebsite23@users.noreply.github.com>
2022-01-19 11:13:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 document.title = "HOME PAGE";
 var button = document.getElementById("button");
-button.onclick =  document.write(Math.floor(Math.random() * 1000));
+if(button.onclick == true){
+  document.write(Math.floor(Math.random() * 1000));
+}
