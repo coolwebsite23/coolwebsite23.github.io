0546cedc57f6e5bfa4fbb81a46d7d4f23104763f
coolwebsite23 <98031434+coolwebsite23@users.noreply.github.com>
2022-01-20 13:04:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-document.title = "HOME PAGE";
-function links(){
-  var div = document.createElement("div");
-  div.innerHTML = ["<ul><li><a href='page2.html'>page 2</a></li></ul>"];
-  document.body.appendChild(div);
+document.title = "HOME PAGE";//add the title
+function links(){//create function
+  var div = document.createElement("div");//create div
+  div.innerHTML = ["<ul><li><a href='page2.html'>page 2</a></li></ul>"];//add links to div
+  document.body.appendChild(div);//append div to body
 }
 if(document.body.onerror){
+   //ask user if he/she wants to stop the debug
    var script = prompt("do you want to turn off JavaScript for this website? (true or false) there has been an error found on the code!");
    if(script == true){
      //the prompt output is true
-     debugger;
+     debugger;//stop debugger
    }else{
      //the prompt output is false
-     console.log("no");
+     console.log("no");//log message to the console
    }
 }
-var scripttag = document.createElement("script");
-script.src = "supported.js";
-document.body.appendChild(script);
-if(getCookie("username")){
-  alert("welcome back" + getCookie("username"));
-}else{
-  console.log("new user");
-}
+//create the script tag
+var scripttag = document.createElement("script");//create the script tag
+scripttag.src = "supported.js";
+document.body.appendChild(scripttag);//append scripttag to body
