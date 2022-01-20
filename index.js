486d1cff16eef3c229134d823cb7b0cd292994da
coolwebsite23 <98031434+coolwebsite23@users.noreply.github.com>
2022-01-20 08:53:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ function links(){
 if(document.body.onerror){
    var script = prompt("do you want to turn off JavaScript for this website? (true or false) there has been an error found on the code!");
    if(script == true){
+     //the prompt output is true
      debugger;
    }else{
+     //the prompt output is false
      console.log("no");
    }
 }
