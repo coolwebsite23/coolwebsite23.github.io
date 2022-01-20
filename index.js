2b8b7c22e6cf3a9c24578102988d3933e70d9e84
coolwebsite23 <98031434+coolwebsite23@users.noreply.github.com>
2022-01-20 10:33:57 -0500

    Create index.js

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ if(document.body.onerror){
      console.log("no");
    }
 }
-var scripttag = document.createElemenr("script");
+var scripttag = document.createElement("script");
 script.src = "supported.js";
 document.body.appendChild(script);
