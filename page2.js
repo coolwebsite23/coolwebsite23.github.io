367b218bab8c079eb136097c09dfdae81bc6b775
coolwebsite23 <98031434+coolwebsite23@users.noreply.github.com>
2022-01-24 09:50:12 -0500

    Create page2.js

--- page2.js
+++ page2.js
@@ -6,6 +6,6 @@ alert("Website loaded");
 if(document.body.onerror){
   window.location.hash = "#js=document.body.onerror";
 }
-if(window.location.hash == "#js=document.body.onerror){
+if(window.location.hash == "#js=document.body.onerror"){
    window.location.assign("error.html#error=document.body.onerror");
 }
