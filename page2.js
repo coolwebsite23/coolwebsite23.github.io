ff3100fd5abd22a0524c189f584c6a24643aa674
coolwebsite23 <98031434+coolwebsite23@users.noreply.github.com>
2022-01-24 09:49:49 -0500

    Update page2.js

--- page2.js
+++ page2.js
@@ -1,4 +1,11 @@
 var titlehead = document.getElementsByTagName("h1");
-if(titlehead.onclick != true){
+if(titlehead.onclick == true){
   window.location.back;
+}
+alert("Website loaded");
+if(document.body.onerror){
+  window.location.hash = "#js=document.body.onerror";
+}
+if(window.location.hash == "#js=document.body.onerror){
+   window.location.assign("error.html#error=document.body.onerror");
 }
